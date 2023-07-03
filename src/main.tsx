2abc28f48c1a7d870cc4857b308122e3f1c55aea
s2s2hyun <s2s2hyun0601@gmail.com>
2023-07-04 01:43:01 +0900

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import App from "./App";
+import About from "./About";
+import "./index.css";
+
+const container = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(container).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
